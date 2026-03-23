92ea4be28cc45fe4fbaa786ce957bcd261dfb76f
grade0ne <alex.mendelson.698@my.csun.edu>
2026-03-23 11:19:10 -0700

    d16 data added

--- scripts/complimentarity/complimD14.R
+++ scripts/complimentarity/complimD14.R
@@ -1,13 +1,14 @@
 library(tidyverse)
 
-complim_data <- read.csv("data/complim_data_d14.csv")
+complim_data <- read.csv("data/complim_data_d16.csv")
 
 rotifer_popn_df <- complim_data %>%
   filter(day != 12) %>%
   mutate(clone = as.factor(clone)) %>%
   group_by(clone, replicate)
 
-ggplot(rotifer_popn_df, aes(x = day, y = rotifers, group = interaction(clone, replicate), color = clone, shape = diversity)) + 
+ggplot(rotifer_popn_df, aes(x = day, y = rotifers, group = interaction(clone, replicate), color = competition, shape = diversity)) + 
   geom_point() +
   geom_line() +
+  facet_wrap(~clone) +
   theme_minimal()

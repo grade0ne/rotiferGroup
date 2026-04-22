5bca9884613556abe77273871604c6109f396db9
grade0ne <alex.mendelson.698@my.csun.edu>
2026-04-22 09:29:34 -0700

    block 2 script and data init

--- scripts/complimentarity/block2.R
+++ scripts/complimentarity/block2.R
@@ -1,10 +1,3 @@
 library(tidyverse)
 
-data <- read.csv("data/block2_prelim1.csv")
-
-plot(rotifers~Day, data)
-
-ggplot(data, aes(x = Day, y = rotifers, group = interaction(clone, Replicates), color = clone)) +
-  geom_point() +
-  geom_line() +
-  facet_wrap(~Treatment)
+data <- read.csv("data/block2data.csv")

9b72bf6b6654bb071826bd57d041d287cb36958f
Breana Morales <breana3@Breanas-MacBook-Pro-1761.local>
2026-04-22 09:58:13 -0700

    block 2 script

--- scripts/complimentarity/block2.R
+++ scripts/complimentarity/block2.R
@@ -1,3 +1,54 @@
 library(tidyverse)
 
-data <- read.csv("data/block2data.csv")
+blk2data <- read.csv("data/block2data.csv")%>%
+  mutate(across(c(diversity, replicate, competition, clone),as.factor))
+
+
+# user-specified model
+logistic_alpha_function <- function(time, parms) {
+  y0    <- parms[["y0"]]
+  r     <- parms[["r"]]
+  alpha <- parms[["alpha"]]
+  
+  y <- (r * y0) / (alpha * y0 + (r - alpha * y0) * exp(-r * time))
+  
+  cbind(time = time, y = y)
+}
+
+grow_logistic_alpha <- growthmodel(
+  logistic_alpha_function,
+  pnames = c("y0", "r", "alpha")
+)
+
+plot_models <- function(models) {
+  coefs <- as.data.frame(coef(models))
+  model_ids <- c(1:length(coefs$y0))
+  
+  columns <- floor(sqrt(length(model_ids)))
+  rows <- columns + 1
+  par(mfrow = c(columns, rows), mar = c(1, 1, 1, 1))
+  
+  for (i in seq_along(model_ids)) {
+    plot(models[[i]])
+    
+    this_r <- round(coefs$r[i], 6)
+    this_a <- round(coefs$alpha[i], 6)
+    this_model <- rownames(coefs)[i]
+    
+    mtext(this_model, side = 3, adj = 0.1, line = -1, cex = 0.5)
+    mtext(paste("r:", this_r), side = 3, adj = 0.1, line = -2, cex = 0.5)
+    mtext(paste("a:", this_a), side = 3, adj = 0.1, line = -3, cex = 0.5)
+  }
+}
+
+rotifer_growth_models <- all_growthmodels(
+  rotifers ~ day | diversity + competition + clone + replicate, 
+  data  = blk2data,
+  p     = c(y0 = 5, r = 0.1, alpha = 0.0005), # initial
+  upper = c(y0 = 7, r = 8, alpha = 1),
+  lower = c(y0 = 0, r = 0.003, alpha = 0.0001),
+  FUN   = grow_logistic_alpha
+)
+
+growth_summary <- results(rotifer_growth_models) %>%
+  mutate(across(c(diversity, competition, clone), as.factor))
